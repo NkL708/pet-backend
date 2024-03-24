9031915d3afbfbcf9818b0909f2d71d16daf4aca
nkl708 <nklnsk708@gmail.com>
2024-03-24 14:46:41 +0700

    Fix endpoints for digest, article
    Add file formatter for VS Code

--- api/urls/__init__.py
+++ api/urls/__init__.py
@@ -1,9 +1,16 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from ..views.article import ArticleViewSet
+from ..views.digest import DigestViewSet
+
+router = DefaultRouter()
+router.register(r"articles", ArticleViewSet)
+router.register(r"digests", DigestViewSet)
 
 urlpatterns = [
     path("token/", include("api.urls.token")),
     path("user/", include("api.urls.user")),
     path("users/", include("api.urls.users")),
-    path("article/", include("api.urls.article")),
-    path("digest/", include("api.urls.digest")),
+    path("", include(router.urls)),
 ]
