55441bde8ee482d20f3fdc356b30256b4864891c
nkl708 <nklnsk708@gmail.com>
2024-03-31 15:55:42 +0700

    Add articles field to DigestSerializer with ArticleSerializer

--- api/serializers/digest.py
+++ api/serializers/digest.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from ..models.digest import Digest
+from ..serializers.article import ArticleSerializer
 
 
 class DigestSerializer(serializers.ModelSerializer):
+    articles = ArticleSerializer(many=True, read_only=True)
+
     class Meta:
         model = Digest
         fields = ["id", "title", "publication_date", "articles"]
