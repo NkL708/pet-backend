fb8cb782d97c235b78494e9d0bf2be8ee00d0f0d
nkl708 <nklnsk708@gmail.com>
2024-05-04 19:01:21 +0700

    Remove User
    User will be rewritten later

--- api/urls/__init__.py
+++ api/urls/__init__.py
@@ -8,9 +8,4 @@ router = DefaultRouter()
 router.register(r"articles", ArticleViewSet)
 router.register(r"digests", DigestViewSet)
 
-urlpatterns = [
-    path("token/", include("api.urls.token")),
-    path("user/", include("api.urls.user")),
-    path("users/", include("api.urls.users")),
-    path("", include(router.urls)),
-]
+urlpatterns = [path("", include(router.urls))]
