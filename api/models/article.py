49d2dfb4826164a5ff2e4160c61c6801c8e92ce6
nkl708 <nklnsk708@gmail.com>
2024-05-04 19:32:46 +0700

    Change max_lenght for source_url and title

--- api/models/article.py
+++ api/models/article.py
@@ -3,6 +3,6 @@ from django.db.models.fields import TextField, URLField
 
 
 class Article(models.Model):
-    source_url: URLField = URLField(max_length=200, verbose_name="Источник")
+    source_url: URLField = URLField(max_length=255, verbose_name="Источник")
     short_text: TextField = TextField(verbose_name="Сокращённый текст")
     full_text: TextField = TextField(verbose_name="Полный текст")
--- api/models/digest.py
+++ api/models/digest.py
@@ -4,7 +4,7 @@ from django.db.models.fields import CharField, DateField
 
 
 class Digest(models.Model):
-    title: CharField = CharField(max_length=255, verbose_name="Заголовок")
+    title: CharField = CharField(max_length=300, verbose_name="Заголовок")
     publication_date: DateField = DateField(verbose_name="Дата публикации")
     articles: ManyToManyField = ManyToManyField(
         "Article", verbose_name="Новости", related_name="digests"
